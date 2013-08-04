f20a3cdc8ecca7f88bdc7fc6e0b58ce96a89281d
mebusw <mebusw@163.com>
2013-08-04 23:29:44 +0800

    add fixtures for tests

---
+++ mysite/polls/tests/test_model.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from polls.models import Poll
+from django.utils import timezone
+
+class PollModelTest(TestCase):
+    def stest_creating_a_new_poll_and_saving_it_to_the_database(self):
+        # start by creating a new Poll object with its "question" set
+        poll = Poll()
+        poll.question = "What's up?"
+        poll.pub_date = timezone.now()
+
+        # check we can save it to the database
+        poll.save()
+
+        # now check we can find it in the database again
+        all_polls_in_database = Poll.objects.all()
+        self.assertEquals(len(all_polls_in_database), 1)
+        only_poll_in_database = all_polls_in_database[0]
+        self.assertEquals(only_poll_in_database, poll)
+
+        # and check that it's saved its two attributes: question and pub_date
+        self.assertEquals(only_poll_in_database.question, "What's up?")
+        self.assertEquals(only_poll_in_database.pub_date, poll.pub_date)
+
+class PollModelWithFixtureTest(TestCase):
+    fixtures = ['polls.json']
+
+    def test_fixtures(self):
+        poll = Poll.objects.get(pk=1)
+        self.assertEquals(3, len(poll.choice_set.all()))
