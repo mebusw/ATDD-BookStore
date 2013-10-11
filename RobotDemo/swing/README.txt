4ffcdfd96f5823680b9884ec52db5b1d39eee89b
mebusw <mebusw@163.com>
2013-10-11 10:47:32 +0800

    add SwingLibrary(Jemmy) demo

---
+++ RobotDemo/swing/README.txt
@@ -0,0 +1,18 @@
+Robot Framework SwingLibrary Demo
+---------------------------------
+
+This directory contains an easily executable demo for Robot Framework
+using SwingLibrary. The tests can be executed using the `run_demo.py`
+script. Full usage instructions are available online at
+http://code.google.com/p/robotframework-swinglibrary/wiki/SwingLibraryDemo
+
+
+* Run from the command line
+
+To start the application for manual exploration, run:
+
+run_demo.py startapp
+
+To run the example Robot Framework tests, run:
+
+run_demo.py example.txt
---
+++ RobotDemo/swing/run_demo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from subprocess import call
+from glob import glob
+
+TESTAPP = 'org.robotframework.swing.testapp.examplesut.TodoListApplication'
+
+def _get_jars():
+    dir = os.path.dirname(__file__)
+    return glob(os.path.join(dir, 'lib',
+                'swinglibrary-*.jar'))
+
+def _set_classpath():
+    os.environ['CLASSPATH'] = os.pathsep.join(_get_jars())
+
+def _parse_command():
+    if sys.argv[1] == 'startapp':
+        return ['java', TESTAPP]
+    return ['jybot', '--outputdir',  'results'] + sys.argv[1:]
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print 'Usage: run_demo.py [startapp|path_to_tests]'
+        sys.exit(1)
+    _set_classpath()
+    call(_parse_command(), shell=os.name=='nt')
