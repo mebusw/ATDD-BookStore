0385c243ef062b87538408a01faa05feaa7f5df3
JackyShen <mebusw@163.com>
2017-03-18 12:11:16 +0800

    format with Pycharm pro

--- BehaveDemo/steps/tutorial.py
+++ BehaveDemo/steps/tutorial.py
@@ -1,18 +1,25 @@
 #!encoding=utf8
 from behave import *
-import sure
 from hamcrest import *
-from _common import *
+import sure
+
+use_step_matcher("re")
 
 con = []
 
-@given('we have behave installed')
+
+@given("we have behave installed")
 def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
     pass
+
 
 @when('we implement a test')
 def step_impl(context):
     assert True is not False
+
 
 @then('behave will test it for us!')
 def step_impl(context):
@@ -24,17 +31,26 @@ def step_impl(context):
     for row in context.table:
         con.append((row['name'], row['department']))
 
+
 @when('login with')
 def step_impl(context):
     pass
 
+
 th = ''
-@given('I put {thing} in a blender,')
+
+
+@given("I put (?P<thing>.+) in a blender,")
 def step_impl(context, thing):
+    """
+    :type context: behave.runner.Context
+    :type thing: str
+    """
     global th
     th = thing
 
-@then('it should transform into {other_thing}')
+
+@then('it should transform into (?P<other_thing>.+)')
 def step_impl(context, other_thing):
     other_thing.should.be.equal('toxic waste')
     assert_that(other_thing, equal_to('toxic waste'))
@@ -46,6 +62,9 @@ def step_impl(context):
         then I duck
     ''')
 
+
 @then('I duck')
 def step_impl(context):
     assert True
+
+
